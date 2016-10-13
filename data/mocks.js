236802aab70f6f2a7b7270de12ef70627dd8643d
mbr84 <matthew.bramfeld@gmail.com>
2016-10-13 11:46:53 -0700

    casual mocks

--- data/mocks.js
+++ data/mocks.js
@@ -1,5 +1,14 @@
+import casual from 'casual';
+
 const mocks = {
-  String: () => 'It works!',
+  String: () => "No it doesn't!",
+  Query: () => ({
+    author: (root, args) => ({
+      firstName: args.firstName, lastName: args.lastName,
+    }),
+  }),
+  Author: () => ({ firstName: () => casual.first_name, lastName: () => casual.last_name }),
+  Post: () => ({ title: () => casual.title, text: () => casual.sentences(3) }),
 };
 
 export default mocks;
